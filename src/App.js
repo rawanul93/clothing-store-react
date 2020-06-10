938130fb958b05f83cc116577cc4eba3ff51a306
Rawanul Momen <rusaf93@gmail.com>
2020-06-11 02:43:05 +0600

    updating the collection preview page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Hats hats</h1>
-  </div>
-);
 
 const JacketsPage = () => (
   <div>
@@ -20,8 +16,8 @@ function App() {
     <div>
       <Switch> {/*switch gives us more power to control which page/component we want to route to. If we have multiple route comps in here, it will stop at one as soon as we get a url match */}
         <Route exact path='/' component={Homepage}/> 
-        <Route path='/shop/hats' component={HatsPage}/> 
-        <Route path='/shop/jackets' component={JacketsPage}/> 
+        <Route exact path='/shop' component={ShopPage}/> 
+        <Route exact path='/shop/jackets' component={JacketsPage}/> 
       </Switch>
       
     </div>
