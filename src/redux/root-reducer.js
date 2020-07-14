72046fff93f7b5ec2c8c07fee38f75d0a9086bb8
Rawanul Momen <rusaf93@gmail.com>
2020-07-15 04:39:23 +0600

    added redux-persist to have session persistence

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
 
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage' //this is the actual localStorage object. We're bascially telling redux-persist that we wanna use our local storage to persist .
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root', //at what point in our reducer object we want to store everything. We want to start at the root.
+    storage, //localStorage.
+    whitelist: ['cart'] //this is an array of the reducers that we want to store in our localStorage.
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
-})
+});
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger'; //helps us debug
+import { persistStore } from 'redux-persist'; //helps us cache our store based on whatever config we set for it.
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger]; //since applyMiddleware takes only one argument, we want to put it in an array and spread it out instead so that its more scalable.
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store); //its just a persisted version of our store.
+
+export default {store, persistor};
