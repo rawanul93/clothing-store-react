fd1ffd7ce3fa95271e0fd77568e410cdd8e16df9
Rawanul Momen <rusaf93@gmail.com>
2020-07-18 09:15:36 +0600

    removed redux-logger from production build and only have it so that it runs during development only

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,12 @@ import { persistStore } from 'redux-persist'; //helps us cache our store based o
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger]; //since applyMiddleware takes only one argument, we want to put it in an array and spread it out instead so that its more scalable.
+// const middlewares = [logger]; //since applyMiddleware takes only one argument, we want to put it in an array and spread it out instead so that its more scalable.
+
+const middlewares = []; //for making sure the logger is not run when the app is hosted live.
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
